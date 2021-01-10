9bb2831ce1f5cf0a45c5b322a8654135aa404fe1
Xavier <71793873+Xeivis@users.noreply.github.com>
2021-01-10 18:05:48 +0100

    Attack
    Added atack fisics (not finished) and base ennemy controller

---
+++ New Confinalistes/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public int health;
+    public int speed;
+    //This is to give a visual efect to the enemy when hit -> public GameObject bloodEffect;
+    private float stunedTime;
+    public float startStunedTime;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(stunedTime <= 0)
+        {
+            speed = 1;
+        }
+        else
+        {
+            speed = 0;
+            stunedTime -= Time.deltaTime;
+        }
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    public void TakeDamage(int damage)
+    {
+        stunedTime = startStunedTime;
+        //This is to give a visual efect to the enemy when hit -> Instantiate(bloodEffect, transform.position, Quaternion.identity);
+        health -= damage;
+    }
+}
---
+++ New Confinalistes/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float timeBtwAttack;
+    public float startTimeBtwAttack;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public int damage;
+
+    private void Update()
+    {
+        if (timeBtwAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                //camAnim.SetTrigger("shake");
+
+                timeBtwAttack = startTimeBtwAttack;
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<EnemyDamage>().TakeDamage(damage);
+                }
+            }
+        }
+        else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
